b9f46c1412b916228173f448c7b7d65c1f9b568a
ash34313 <ashrensau@gmail.com>
2026-04-20 17:19:12 +0000

    4-20 update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,5 +7,37 @@ import AppointmentsPage from './page/AppointmentsPage'
 function App(){
   const [currentPage, setCurrentPage] = useState('dashboard')
 
-  return()
+  return(
+    <div className="min-h-screen bg-gray-50">
+      <nav className="bg-white shadow p-4">
+        <div className="max-w-7x1 mx-auto flex gap-6">
+          <button className="font-medium hover:text-blue-600" onClick={()=>setCurrentPage('dashboard')}>
+            Dashboard
+          </button>
+
+          <button className="font-medium hover:text-blue-600" onClick={()=>setCurrentPage('patients')}>
+            Patients
+          </button>
+
+          <button className="font-medium hover:text-blue-600" onClick={()=>setCurrentPage('doctors')}>
+            Doctors
+          </button>
+
+          <button className="font-medium hover:text-blue-600" onClick={()=>setCurrentPage('appointments')}>
+            Appointments
+          </button>
+        </div>
+      </nav>
+
+      <main className="max-w-7x1 mx-auto p-6">
+        {currentPage==='dashboard' && <Dashboard/>}
+        {currentPage==='patients' && <PatientsPage/>}
+        {currentPage==='doctors' && <DoctorsPage/>}
+        {currentPage==='appointments' && <AppointmentsPage/>}
+      </main>
+
+    </div>
+  )
 }
+
+export default App;
